2ff374f3871785b361389c2fac56b7e70029c11c
Thereka <162158931+Thereka@users.noreply.github.com>
2025-07-17 11:42:01 +0530

    Create operators.js

---
+++ bascis/operators.js
@@ -0,0 +1,42 @@
+let x=5;
+let y=10;
+console.log("Arithmetic opertator  ");
+console.log("Addition : "+(x+y));
+console.log("Addition : "+(x-y));
+console.log("Addition : "+(x*y));
+console.log("Addition : "+(x/y));
+
+console.log("Assignment opertator  ");
+var a1=20;
+var b1=30;
+console.log(a1+=5);
+console.log(b1-=5);
+console.log(a1*=5);
+console.log(b1/=5);
+
+console.log("Comparision opertator  ");
+console.log(x==y);
+console.log(x>y);
+console.log(x<y);
+console.log(x>=y);
+console.log(x<=y);
+
+console.log("Logical operator ");
+console.log(x&&y);
+console.log(x||y);
+console.log(!x);
+
+console.log("String operator ");
+let a="Hello";
+let b="Everyone!"
+console.log(a+" "+b);
+
+console.log("Ternary operetor ");
+let age=19;
+let vote=age>=18? "Eligible to vote" :"Not eligible to vote";
+console.log(vote);
+
+
+
+
+
