f1eb0346af826711ff47ac9273c9301340f5f8d5
Thereka <162158931+Thereka@users.noreply.github.com>
2025-07-18 12:28:21 +0530

    Update functions .js

--- bascis/functions .js
+++ bascis/functions .js
@@ -13,3 +13,42 @@ function Perimeter(l,w){
 }
 Perimeter(20,10);
 
+//if condition in function
+function Vote( age){
+    if(age>=18){
+        console.log("Eligible to vote");
+    }else{
+        console.log("Not Eligible to vote");
+    }
+}
+Vote(22);
+
+//for loop -functions
+function Number(num){
+    for(let i=0;i<num;i++){
+        console.log(i);
+    }
+}
+let num=prompt("Enter the Number : ");
+Number(num);
+
+//while
+function Number(n){
+    let rev=0;
+    while(n!=0){
+        let l=n%10;
+        rev=rev*10+l;
+        n=Math.floor(n/10);
+    }
+    console.log(rev);
+}
+Number(1234);
+
+// return function
+function Greet(){
+    return "Hello Everyone";
+}
+let a=Greet();
+console.log(a);
+
+
