ace1f559c3b909119e605d076f0859175528f4b3
Thereka <162158931+Thereka@users.noreply.github.com>
2025-07-18 11:56:08 +0530

    Update functions .js

--- bascis/functions .js
+++ bascis/functions .js
@@ -1,4 +1,15 @@
+//ares of Rectangle
+
 function Area(len,bre){
     console.log("Area of Rectangle :"+len*bre);
 }
 Area(5,6);
+
+
+//Perimetr of Recatngle
+
+function Perimeter(l,w){
+    console.log("Perimeter of rectangle: "+(2*(l+w)));
+}
+Perimeter(20,10);
+
